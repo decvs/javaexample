cfaabcc45ad86e5d642fcac2fbeaaaa685f177bf
decvs <flyff.sebulba@gmail.com>
2016-06-26 10:31:30 +0200

    mit user

---
+++ UserExample/src/user/User.java
@@ -0,0 +1,26 @@
+package user;
+
+public class User {
+	private String name;
+	private String password;
+	private int position;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+}
